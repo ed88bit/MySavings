b71c23bc833ee6faa30543b958b327cb1164efbc
e3elauri <Accenture.Laurini@Consulenti.GeneraliGroup.com>
2021-07-28 18:16:51 +0200

    [MYS - 11] - automation for managing new records directly from excel

--- src/main/java/my/mys/domain/Movements.java
+++ src/main/java/my/mys/domain/Movements.java
@@ -1,29 +1,31 @@
 package my.mys.domain;
 
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
+@Builder
 @Table(name = "movements", schema="mys")
 public class Movements {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "ID")
     private int id;
 
     private LocalDate accounting_date;
     private Integer competence_month;
-    private double negative_amounts;
-    private double positive_amounts;
+    private Integer competence_year;
+    private Double negative_amounts;
+    private Double positive_amounts;
     private String detail;
 
 }
--- src/main/java/my/mys/service/DatabaseLoaderService.java
+++ src/main/java/my/mys/service/DatabaseLoaderService.java
@@ -1,6 +1,7 @@
 package my.mys.service;
 
 import lombok.AllArgsConstructor;
+import my.mys.domain.Movements;
 import my.mys.repository.MovementsRepository;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
@@ -10,6 +11,10 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.tomcat.jni.File;
+import org.hibernate.Session;
+import org.hibernate.SessionBuilder;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,6 +23,9 @@ import java.io.InputStream;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.nio.file.Path;
+import java.text.SimpleDateFormat;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.Iterator;
 
 @Service
@@ -32,6 +40,7 @@ public class DatabaseLoaderService {
             InputStream inputStream = inputFile.getInputStream();
             POIFSFileSystem poifsFileSystem = new POIFSFileSystem(inputStream);
             HSSFWorkbook workbook = new HSSFWorkbook(poifsFileSystem);
+            Movements movementsFromExcel = new Movements();
 
             //Getting the first sheet of the workbook
             HSSFSheet sheet = workbook.getSheetAt(0);
@@ -44,31 +53,69 @@ public class DatabaseLoaderService {
                 System.out.print("******************");
                 System.out.print("Line Number: "+rowIteratorCount);
                 System.out.print("******************");
-                rowIteratorCount++;
                 System.out.print(newLine);
                 Row row = rowIterator.next();
-                //For each row, iterate through all the columns
-                Iterator<Cell> cellIterator = row.cellIterator();
-                int cellIteratorCount = 0;
-                while (cellIterator.hasNext()) {
-                    System.out.print("Cell (column) Number: "+rowIteratorCount+" ");
-                    Cell cell = cellIterator.next();
-                    //Check the cell type and format accordingly, other cell types exist, you may want to check them out
-                    switch (cell.getCellType()) {
-                        case NUMERIC:
-                            //if()
-                            System.out.print(cell.getNumericCellValue()+"||");
-                            break;
-                        case STRING:
-                            System.out.print(cell.getStringCellValue()+"||");
-                            break;
+                if(rowIteratorCount > 0) {
+                    //For each row, iterate through all the columns
+                    Iterator<Cell> cellIterator = row.cellIterator();
+                    int cellIteratorCount = 0;
+                    while (cellIterator.hasNext()) {
+                        System.out.print("Cell (column) Number: "+cellIteratorCount+" ");
+                            Cell cell = cellIterator.next();
+                            //Check the cell type and format accordingly, other cell types exist, you may want to check them out
+                            switch (cell.getCellType()) {
+                                case NUMERIC:
+                                    switch (cellIteratorCount) {
+                                        case 0:
+                                            movementsFromExcel.setAccounting_date(cell.getLocalDateTimeCellValue().toLocalDate());
+                                            break;
+                                        case 1:
+                                            movementsFromExcel.setNegative_amounts(cell.getNumericCellValue());
+                                            movementsFromExcel.setPositive_amounts(null);
+                                            break;
+                                        case 2:
+                                            movementsFromExcel.setPositive_amounts(cell.getNumericCellValue());
+                                            movementsFromExcel.setNegative_amounts(null);
+                                            break;
+                                    }
+                                    System.out.print(cell.getNumericCellValue() + " << numeric||");
+                                    cellIteratorCount++;
+                                    break;
+                                case STRING:
+                                    switch (cellIteratorCount) {
+                                    case 3:
+                                        movementsFromExcel.setDetail(cell.getStringCellValue());
+                                        break;
+                                }
+                                    System.out.print(cell.getStringCellValue() + " << string||");
+                                    cellIteratorCount++;
+                                    break;
+                                default:
+                                    System.out.print(cell.getStringCellValue() + " << something else||");
+                                    cellIteratorCount++;
+                                    break;
+                            }
+                        }
+                    Movements movements = Movements.builder()
+                            .accounting_date(movementsFromExcel.getAccounting_date())
+                            .competence_month(isAfterTheTwentySeventh(movementsFromExcel) ? movementsFromExcel.getAccounting_date().getMonthValue()+1:movementsFromExcel.getAccounting_date().getMonthValue())
+                            .competence_year(movementsFromExcel.getAccounting_date().getYear())
+                            .negative_amounts(movementsFromExcel.getNegative_amounts())
+                            .positive_amounts(movementsFromExcel.getPositive_amounts())
+                            .detail(movementsFromExcel.getDetail())
+                            .build();
+                    movementsRepository.save(movements);
                     }
-                }
-
+                rowIteratorCount++;
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
         return "ok";
+    }
+
+    private boolean isAfterTheTwentySeventh(Movements movementsFromExcel) {
+        return ((movementsFromExcel.getAccounting_date().getDayOfMonth() >= 25 && null!=movementsFromExcel.getPositive_amounts())
+                ||(movementsFromExcel.getAccounting_date().getDayOfMonth() >= 27 && null!=movementsFromExcel.getNegative_amounts()));
     }
 }
